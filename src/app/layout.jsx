15e11b28261ef18e95d9fb5a981889d6fae5cf8b
CopyCatMe <wiskripper@gmail.com>
2024-11-16 00:39:11 +0100

    Intentando coseguir las paginas borradas.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-=======
 'use client';
->>>>>>> a006c7dfdb5dc8d911a809511cc598b7c4401f6a
+
 import localFont from "next/font/local";
 import "./globals.css";
 import MenuPrincipal from "@/components/menu-principal.jsx";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a006c7dfdb5dc8d911a809511cc598b7c4401f6a
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,29 +18,14 @@ const geistMono = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex h-screen`}>
-          <aside class="shrink-0">
-          <MenuPrincipal/>
-
-          </aside>
-        <main className="h-full grow">
-        {children}
-=======
       <body className={`${geistSans.variable} antialiased flex flex-col lg:flex-row w-full h-screen`}>
 
           <MenuPrincipal />
 
         <main className=" flex-1 h-full">
           {children}
->>>>>>> a006c7dfdb5dc8d911a809511cc598b7c4401f6a
         </main>
       </body>
     </html>
   );
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> a006c7dfdb5dc8d911a809511cc598b7c4401f6a
--- src/components/titulo.jsx
+++ src/components/titulo.jsx
@@ -1,14 +1,8 @@
 function Titulo({children}) {
     return (  
-<<<<<<< HEAD
-        <div className="text-3xl font-bold underline text-center text-blue-500 py-10 text-5xl font-bold">
-            {children}
-        </div>
-=======
         <h1 className="mt-3 mb-7 text-4xl text-blue-500 font-bold py-2 border-b-4 border-blue-500">
             {children}
         </h1>
->>>>>>> a006c7dfdb5dc8d911a809511cc598b7c4401f6a
     );
 }
 
